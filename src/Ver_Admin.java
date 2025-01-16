fe3700a87369c0c9019ae79b1d87cc382de78ef4
RichardPadilla7 <richardpadilla3rod@gmail.com>
2025-01-16 18:48:25 -0500

    Visualizar

---
+++ src/Ver_Admin.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ver_Admin {
+    public JTabbedPane tabbedPane1;
+    public JPanel visualito;
+    public JLabel VerDatosProductos;
+    public JButton verButton;
+    private JButton regresarButton;
+
+    public Ver_Admin() {
+        verButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+
+
+
+            }
+        });
+
+
+
+
+
+
+        regresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+
+
+
+            }
+        });
+    }
+}
